6db95d808396e858dccb89c41cb8e15c79b4ab82
Daniel B <daniborgesmarque@gmail.com>
2022-01-05 10:19:08 -0300

    Upload dos arquivos

---
+++ FrontEnd2/FrontEnd2/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿namespace FrontEnd2.Models
+{
+    public class Genre
+    {
+        public Genre()
+        {
+            Movie = new HashSet<Movie>();
+        }
+
+        public int GrId { get; set; }
+        public string Genre1 { get; set; } = null!;
+
+        public virtual ICollection<Movie> Movie { get; set; }
+    }
+}
---
+++ FrontEnd2/FrontEnd2/Models/VwMoviesGenre.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontEnd2.Models
+{
+    public class VwMoviesGenre
+    {
+        public string? MvTitle { get; set; }
+        public string? MvDate { get; set; }
+        public string? Genre { get; set; }
+        public string? MvImg { get; set; }
+        public int? MvDuration { get; set; }
+        public string? MvSynopsis { get; set; }
+
+    }
+}
